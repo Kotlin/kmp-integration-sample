92da18d0e352f1bd69f59c80a3f5c7866ed2f616
Danil Pavlov <danil.pavlov@jetbrains.com>
2023-03-23 15:03:03 +0100

    fix: dependency on shared

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ android {
 }
 
 dependencies {
+    implementation (project(":shared"))
     implementation ("androidx.core:core-ktx:1.9.0")
     implementation ("androidx.appcompat:appcompat:1.6.1")
     implementation ("com.google.android.material:material:1.8.0")
