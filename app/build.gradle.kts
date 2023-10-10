f047722ea72a0ddcf26c9c1ab996e89ac142c88f
Konstantin Tskhovrebov <konstantin.tskhovrebov@jetbrains.com>
2023-10-10 16:26:57 +0200

    Update Kotlin to 1.9.20-Beta2 and other deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 }
 
 android {
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.jetbrains.simplelogin.androidapp"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -40,13 +40,13 @@ android {
 
 dependencies {
 
-    implementation ("androidx.core:core-ktx:1.9.0")
+    implementation ("androidx.core:core-ktx:1.12.0")
     implementation ("androidx.appcompat:appcompat:1.6.1")
-    implementation ("com.google.android.material:material:1.8.0")
-    implementation ("androidx.annotation:annotation:1.6.0")
+    implementation ("com.google.android.material:material:1.10.0")
+    implementation ("androidx.annotation:annotation:1.7.0")
     implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.6.0")
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.0")
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     testImplementation ("junit:junit:4.13.2")
     androidTestImplementation ("androidx.test.ext:junit:1.1.5")
     androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,5 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "7.4.2" apply false
-    id("com.android.library") version "7.4.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
-}
-
-tasks {
-    register("clean", Delete::class) {
-        delete(rootProject.buildDir)
-    }
+    //trick: for the same plugin versions in all sub-modules
+    id("com.android.application").version("8.1.2").apply(false)
+    kotlin("android").version("1.9.20-Beta2").apply(false)
 }
