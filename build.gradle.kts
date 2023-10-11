b8f3b04bb15506eefd91c81241a2778c42c27562
Konstantin Tskhovrebov <konstantin.tskhovrebov@jetbrains.com>
2023-10-11 10:46:51 +0200

    Update Kotlin to 1.9.20-RC

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
     //trick: for the same plugin versions in all sub-modules
     id("com.android.application").version("8.1.2").apply(false)
-    kotlin("android").version("1.9.20-Beta2").apply(false)
+    kotlin("android").version("1.9.20-RC").apply(false)
 }
