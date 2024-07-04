472938cebfadd10aed80799e10d60f131f219f87
Aleksey.Zamulla <aleksey.zamulla@jetbrains.com>
2024-07-04 20:41:41 +0200

    fix: less error-prone framework name

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -18,7 +18,7 @@ kotlin {
         iosSimulatorArm64()
     ).forEach {
         it.binaries.framework {
-            baseName = "shared"
+            baseName = "Shared"
             isStatic = true
         }
     }
