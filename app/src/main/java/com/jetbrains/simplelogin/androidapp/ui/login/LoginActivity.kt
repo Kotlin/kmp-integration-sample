655edb3c01d9cba7e93bb2399d49d37b664c7fd5
pahill <pamela.a.hill@gmail.com>
2025-06-29 18:38:37 +0200

    Update dependencies + wizard to June 2025's versions.

--- app/src/main/java/com/jetbrains/simplelogin/androidapp/ui/login/LoginActivity.kt
+++ app/src/main/java/com/jetbrains/simplelogin/androidapp/ui/login/LoginActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -18,6 +19,7 @@ import com.jetbrains.simplelogin.shared.data.LoginRepository
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
         Log.i("Login Activity", "Hello from shared module: " + (Greeting().greet()))
